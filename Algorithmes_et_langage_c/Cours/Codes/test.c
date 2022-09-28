cc23eb3fd5e49e99df155b093ea79aafc0b3bff2
pilip <guillemota@outlook.fr>
2022-09-28 16:12:20 +0200

    a

--- Algorithmes_et_langage_c/Cours/Codes/test.c
+++ Algorithmes_et_langage_c/Cours/Codes/test.c
@@ -1,23 +1,6 @@
 #include<stdio.h>
-
-int increment()
-{
-	static int c=4;
-	c++;
-	return c;
-}
-
-int decrement()
-{
-	static int c=4;
-	c--;
-	return c;
-}
-
 int main()
 {
-	increment();
-	decrement();
-	printf("%d\n", increment());
-	printf("%d\n", decrement());
+	int a;
+	printf("%d", a);
 }
